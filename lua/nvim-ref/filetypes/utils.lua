cc81cd6a302425ba0731ea95a97fac8f43d11a77
oncomouse <oncomouse@gmail.com>
2026-04-23 12:51:32 -0500

    fix: handle __index correctly

--- lua/nvim-ref/filetypes/utils.lua
+++ lua/nvim-ref/filetypes/utils.lua
@@ -2,16 +2,11 @@ local M = {}
 
 function M.setmetatable(table)
 	return setmetatable(table, {
-		__index = function(t, idx)
-			if idx == "find_start" then
-				return function()
+    __index = {
+      find_start = function()
 					return require("nvim-ref.filetypes").find_start(t.start_pattern or [[\k\+]])
 				end
-			end
-			return t[idx] and t[idx] or function()
-				return nil
-			end
-		end,
+    },
 	})
 end
 return M
