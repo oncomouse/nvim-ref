d2fbba4b95a5cc0f1bae19858a285f15dc7bb879
oncomouse <oncomouse@gmail.com>
2026-04-27 11:48:28 -0500

    chore(config): type check

--- lua/nvim-ref/config.lua
+++ lua/nvim-ref/config.lua
@@ -1,3 +1,4 @@
+local check_type = require("nvim-ref.utils.validate").check_type
 local default_config = {
   include_pagenumbers = false,
 	bibfiles = {},
@@ -15,9 +16,15 @@ local default_config = {
 	lsp = {},
 }
 local function config(opts)
+  check_type("opts", opts, "table")
 	if opts.bibfiles and type(opts.bibfiles) == "string" then
 		opts.bibfiles = { opts.bibfiles }
 	end
+  check_type("opts.bibfiles", opts.bibfiles, "table")
+  check_type("opts.include_pagenumbers", opts.include_pagenumbers, "boolean", true)
+  check_type("opts.commands", opts.commands, "table", true)
+  check_type("opts.filetypes", opts.filetypes, "table", true)
+  check_type("opts.lsp", opts.lsp, "table", true)
 	return vim.tbl_deep_extend("keep", opts, default_config)
 end
 
